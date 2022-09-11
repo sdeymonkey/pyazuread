28b1526eb8e9c31895743221c1f1e9d565d89595
Suman Dey <sdey@momentive.ai>
2022-09-12 00:44:09 +0530

    Upload

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup
+import pathlib
+import pkg_resources
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(name='pyazuread',
     version='0.1',
+    install_requires=install_requires,
     description='This package for verify Azure SSO JWT token to rest API.',
     author='Suman Dey',
     author_email='email.sumandey@gmail.com',
